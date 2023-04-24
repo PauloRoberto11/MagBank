720aad919162ca32abef8cff82ea8ee319e6b1dd
Paulo Silva <plugpaulo@hotmail.com>
2023-04-24 17:28:53 -0300

    Adição Navbar e inicio do Hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+import CenteredButton from './components/CenteredButton';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Hero />
+      <CenteredButton>
+        Abra sua conta
+      </CenteredButton>
     </div>
   );
 }
---
+++ src/components/CenteredButton.js
@@ -0,0 +1,17 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+
+const CenteredButton = ({children}) => {
+    return (
+        <Container>
+            <Row className="d-flex justify-content-center">
+                <Button className="my-5 px-5 py-3" variant="success" size="lg">
+                    {children}
+                </Button>
+            </Row>
+        </Container>
+    )
+
+}
+export default CenteredButton;
