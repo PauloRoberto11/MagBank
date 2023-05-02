cb39937f0592d2751574c355a9e59950bf2e6079
Paulo Silva <plugpaulo@hotmail.com>
2023-05-02 10:11:05 -0300

    finalizando footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,42 @@
 import { Col, Row, Container, Image} from "react-bootstrap";
+import { faFacebookSquare, faTwitter, faYoutube} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import logo from "../assets/MAGbank.png";
 import applestore from "../assets/applestore.png";
 import googleplay from "../assets/googleplay.png";
+import "./Footer.scss";
 
-import { faFacebookSquare, faTwitter, faYoutuber} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "fortawesome/react-fontawesome";
 
 const Footer = () => {
     return (
-        <Container>
-            <Row>
-                <Col xs={12} lg={6}>
-                    <Image src={logo} />
-                </Col>
-                    
-                <Col xs={12} lg={4}>
-                    <Image src={applestore} />
-                    <Image src={googleplay} className='ms-4'  />
-                </Col>
+        <Footer className='Footer'>
+            <Container>
+                <Row className= 'text-center py-5'>
+                    <Col xs={12} lg={6} className="text-lg-start">
+                        <Image src={logo} />
+                    </Col>
                         
-                <Col xs={12} lg={2}>
-                    <a href='https://www.facebook.com' target="_blank" rel="noreferrer">
-                        <FontAwesomeIcon icon={faFacebookSquare} color="7c7c7c" size= {2} />
-                    </a>
-                    <a href='https://www.twitter.com' target="_blank" rel="noreferrer">
-                        <FontAwesomeIcon icon={faTwitter} color="7c7c7c" size= {2} className='ms-2' />
-                    </a>
-                    <a href='https://www.youtuber.com' target="_blank" rel="noreferrer">
-                        <FontAwesomeIcon icon={faYoutuber} color="7c7c7c" size= {2} className='ms-2' />
-                    </a>
-                </Col>
+                    <Col xs={12} lg={4} classname=''>
+                        <Image src={applestore} />
+                        <Image src={googleplay} className='ms-3 my-3 my-lg-0' />
+                    </Col>
+                            
+                    <Col xs={12} lg={2}>
+                        <a href='https://www.facebook.com' target="_blank" rel="noreferrer">
+                            <FontAwesomeIcon icon={faFacebookSquare} size= '2x' />
+                        </a>
+                        <a href='https://www.twitter.com' target="_blank" rel="noreferrer">
+                            <FontAwesomeIcon icon={faTwitter} size= '2x' className='ms-2' />
+                        </a>
+                        <a href='https://www.youtube.com' target="_blank" rel="noreferrer">
+                            <FontAwesomeIcon icon={faYoutube} color="#c4302b" size= '2x' className='ms-2' />
+                        </a>
+                    </Col>
 
-            </Row>
-        </Container>
+                </Row>
+            </Container>
+        </Footer>
     )
 
 }
